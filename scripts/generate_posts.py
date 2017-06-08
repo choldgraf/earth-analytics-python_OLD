b52dde2b37c05e960e9cfbcd25bbc6d1df1c4ae7
Chris Holdgraf <choldgraf@gmail.com>
2017-06-07 19:01:22 -0700

    adding site.url functionality

--- scripts/generate_posts.py
+++ scripts/generate_posts.py
@@ -7,86 +7,106 @@ corresponding jekyll markdown files.
 """
 import nbformat as nf
 import os
+import os.path as op
 import subprocess
 from glob import glob
 from tqdm import tqdm
 from datetime import datetime
 import shutil as sh
 
+
+def parse_meta_file(meta, run_keys={}):
+    """Parse a metadata cell of a Jupyter notebok."""
+    if meta['cell_type'] != 'raw':
+        raise ValueError('First cell must have raw text inside')
+    # A dictionary that runs the function with the value as input
+    def_run_keys = {'modified': lambda a: eval(a.format(datetime.today())),  # Insert the date
+                    'permalink': lambda a: a.replace('earth-analytics/', 'earth-analytics-python/')  # Correct URL paths
+                    }
+    run_keys.update(def_run_keys)
+
+    # Only use lines with key: val pairs
+    metalines = meta['source'].split('\n')
+    for ii, ln in enumerate(metalines):
+        if '---' in ln:
+            continue
+        split = ln.split(': ')
+        if len(split) < 2:
+            continue
+        if len(split) > 2:
+            ln_type = split[0]
+            ln_val = ': '.join(split[1:])
+        else:
+            ln_type, ln_val = split
+
+        # If we have a function for one of the keys, run it and replace the value
+        if ln_type not in run_keys.keys():
+            continue
+        new_val = run_keys[ln_type](ln_val)
+        metalines[ii] = ': '.join([ln_type, new_val])
+
+    # Replace the meta cell
+    meta['source'] = '\n'.join(metalines)
+    return meta
+
 # --- Set up paths ---
 # Path to root of site
-path_root = os.path.abspath('../')
+path_root = op.abspath('../')
 
 # Finds the ipynb files
-files = glob('../notebooks/final-notebooks/*/*/*.ipynb')
-
-# A dictionary that runs the function with the value as input
-run_keys = {'modified': lambda a: a.format(datetime.today())}
+files = glob(op.join('..', 'notebooks', 'final-notebooks', '*', '*', '*.ipynb'))
 
 # Where to save the MD posts
-save_path = '../_posts/course-materials/earth-analytics-python/'
+save_path = op.join('..', '_posts', 'course-materials', 'earth-analytics-python')
 
 
 # --- Preprocess Jupyter Notebooks ---
 # Temporary directory for jupyntbks
-if not os.path.isdir(os.path.join('.', 'tmp')):
-    os.makedirs(os.path.join('.', 'tmp'))
+if not op.isdir(op.join('.', 'tmp')):
+    os.makedirs(op.join('.', 'tmp'))
 
 built, skipped = (0, 0)
 for ifile in tqdm(files):
     # Find file names etc
     path = ifile.split('final-notebooks/')[-1]
-    path_tmp_file = os.path.basename(path)
-    file_name = os.path.splitext(path_tmp_file)[0]
-    path_folders = os.path.dirname(path)
-    path_rel_to_root = os.path.dirname(ifile.split('final-notebooks')[1])
+    path_tmp_file = op.basename(path)
+    file_name = op.splitext(path_tmp_file)[0]
+    path_folders = op.dirname(path)
+    path_rel_to_root = op.dirname(ifile.split('final-notebooks')[1])
 
+    # --- Metadata ---
     # Read in notebook, pull meta cell
     ntbk = nf.read(ifile, nf.NO_CONVERT)
     meta = ntbk['cells'][0]
-    if meta['cell_type'] == 'raw':
+    ntbk['cells'][0] = parse_meta_file(meta)
 
-        # Only use lines with key: val pairs
-        metalines = meta['source'].split('\n') 
-        for ii, ln in enumerate(metalines):
-            if '---' in ln:
-                continue
-            split = ln.split(': ')
-            if len(split) < 2:
-                continue
-            if len(split) > 2:
-                ln_type = split[0]
-                ln_val = ': '.join(split[1:])
-            else:
-                ln_type, ln_val = split
+    # --- Replace cell text ---
+    # Change image paths to {{ site.url }}
+    # Path relative to root for this notebook
+    rel_path = os.path.relpath(path_root, ifile)
 
-            # If we have a function for one of the keys, run it and replace the value
-            if ln_type not in run_keys.keys():
-                continue
-            new_val = eval(run_keys[ln_type](ln_val))
-            metalines[ii] = ': '.join([ln_type, new_val])
-
-        # Replace the meta cell
-        meta['source'] = '\n'.join(metalines)
-        ntbk['cells'][0] = meta
+    for ii, cell in enumerate(ntbk['cells']):
+        if cell['cell_type'] == 'markdown':
+            cell['source'] = cell['source'].replace(rel_path[:-2], "{{ site.url }}%s" %os.sep)
 
     # --- Convert ---
     # Set paths
-    build_path = os.path.join(path_root, "_posts/course-materials/earth-analytics-python/{}".format(path_rel_to_root))
-    path_imgs_root = os.path.relpath(path_root, build_path)
-    images_path = os.path.join(path_imgs_root, "images/course-materials/earth-analytics-python/{}".format(path_rel_to_root))
-    path_new_file = os.path.join(build_path, file_name + '.md')
+    path_rel_to_root_split = path_rel_to_root.split('/')
+    build_path = op.join(path_root, "_posts", 'course-materials', 'earth-analytics-python', *path_rel_to_root_split)
+    path_imgs_root = op.relpath(path_root, build_path)
+    images_path = op.join(path_imgs_root, "images", "course-materials", "earth-analytics-python", *path_rel_to_root_split)
+    path_new_file = op.join(build_path, file_name + '.md')
 
     # Check the file date, if md has been modified *after* notebook then don't re-build it
-    if os.path.exists(path_new_file):
-        last_modified_ntbk = os.path.getmtime(ifile)
-        last_modified_md = os.path.getmtime(path_new_file)
+    if op.exists(path_new_file):
+        last_modified_ntbk = op.getmtime(ifile)
+        last_modified_md = op.getmtime(path_new_file)
         if last_modified_ntbk <= last_modified_md:
             skipped += 1
             continue
 
     # Write the file
-    path_save_tmp_file = os.path.join('.', 'tmp', path_tmp_file)
+    path_save_tmp_file = op.join('.', 'tmp', path_tmp_file)
     nf.write(ntbk, path_save_tmp_file)
 
     # Call nbconvert
@@ -99,5 +119,5 @@ for ifile in tqdm(files):
     # Clean up
     os.remove(path_save_tmp_file)
     built += 1
-sh.rmtree(os.path.join('.', 'tmp'))
+sh.rmtree(op.join('.', 'tmp'))
 print('Built {} files\nSkipped {} files'.format(built, skipped))
